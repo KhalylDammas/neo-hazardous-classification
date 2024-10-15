624f2b23dfbf24c0acca41dbb3c7aec4714e4521
khalyl alghamdi <khalyl.ibraheem@outlook.com>
2024-10-15 16:10:43 +0300

    Assigning all environment variables to variables in a python file to be used throughout the project package

---
+++ neo/params.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+
+######################## VARIABLES ########################
+RANDOM_STATE = int(os.environ.get('RANDOM_STATE'))
+DATA_LOCAL_PATH = os.environ.get('DATA_LOCAL_PATH')
+
+PREFECT_FLOW_NAME = os.environ.get("PREFECT_FLOW_NAME")
+PREFECT_LOG_LEVEL = os.environ.get("PREFECT_LOG_LEVEL")
+
+GAR_IMAGE = os.environ.get("GAR_IMAGE")
+GAR_MEMORY = os.environ.get("GAR_MEMORY")
