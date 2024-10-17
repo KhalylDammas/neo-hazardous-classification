cab7525a8befba788659cbdd27c3cfa11494ae7d
khalyl alghamdi <khalyl.ibraheem@outlook.com>
2024-10-17 08:31:02 +0300

    Remove unnecessary lines of code, Reordering some processes, general refinment

--- neo/interface/main.py
+++ neo/interface/main.py
@@ -1,57 +1,54 @@
+import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error
 
 from neo.params import *
-from neo.ml_logic.data import  clean_data, load_data
-from neo.ml_logic.model import initialize_model, compile_model, train_model, evaluate_model
-from neo.ml_logic.preprocessor import preprocess_features
-from neo.ml_logic.registry import load_model, save_model, save_results
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+
+from neo.ml_logic.data import  preprocessing, clean_data
+from neo.ml_logic.model import train_model, evaluate_model
 from neo.ml_logic.registry import mlflow_run, mlflow_transition_model
+from neo.ml_logic.registry import load_model, save_model, save_results
+# from neo.ml_logic.preprocessor import preprocess_features     # Not yet exists...
 
 
 def preprocess():
-    data = load_data()
 
-    cleaned_data = clean_data(data)
-    return cleaned_data
+    data = pd.read_csv(DATA_LOCAL_PATH)
 
-
-
-
-
-def train_model(X_train, y_train):
-    model = LinearRegression()  # Simple linear regression model
-    model.fit(X_train, y_train)
-    return model
-
-def evaluate_model(model, X_test, y_test):
-    y_pred = model.predict(X_test)
-    mae = mean_absolute_error(y_test, y_pred)  # Calculate mean absolute error
-    return mae
-
-def predict(model, X_new):
-    return model.predict(X_new)
-
-if __name__ == '__main__':
-    # Load the data
-    data = load_data()
     data = clean_data(data)
 
-    # Prepare the data
-    X = data[['absolute_magnitude','estimated_diameter_min','relative_velocity','miss_distance']]
-    y = data['is_hazardous']
+    data = preprocessing(data)
 
-    # Split the data into training and validation sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return data
 
-    # Train the model
-    model = train_model(X_train, y_train)
 
-    # Evaluate the model
-    mae = evaluate_model(model, X_test, y_test)
-    print(f'MAE: {mae}')
+def train():
+    # TODO
+    data = pd.read_csv(DATA_LOCAL_PATH)
+
+    val_results = train_model()
+    return val_results
+
+def evaluate():
+    # TODO
+    return #score
+
+def predict(model, X_new):
+    # TODO
+    return #model.predict(X_new)
+
+
+
+if __name__ == '__main__':
+
+    # TODO
+    preprocess()
+    train()
+    evaluate()
 
     # Predict on new data
     X_new = pd.DataFrame({
