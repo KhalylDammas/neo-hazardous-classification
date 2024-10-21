683a0b563bfdf977f5884bebfc60c95c86b6cce5
khalyl alghamdi <khalyl.ibraheem@outlook.com>
2024-10-21 13:29:45 +0300

    Nothing to note. The DNN is just diappointing man...

---
+++ neo/ml_logic/dnn.py
@@ -0,0 +1,10 @@
+# import numpy as np
+
+# import torch
+# from torch import nn
+# from torch.optim import Adam
+# from torch.functional import
+# from torch.utils.data import DataLoader, Dataset
+
+
+# Unseccessful...
