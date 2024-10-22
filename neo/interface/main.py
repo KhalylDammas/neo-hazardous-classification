6fc3c0f46edb2770ebd7e748d84f256bd5e145bc
khalyl alghamdi <khalyl.ibraheem@outlook.com>
2024-10-22 12:01:55 +0300

    Reworked 'preprocess' method to allow fit on the training data only and transform on the rest.

--- neo/interface/main.py
+++ neo/interface/main.py
@@ -7,39 +7,71 @@ from neo.ml_logic.model import *
 
 from sklearn.model_selection import train_test_split
 
-def preprocess(test=True):
-
-    data = pd.read_csv(DATA_LOCAL_PATH)
-
+def preprocess(data:pd.DataFrame, transformer:object, fit=False) -> tuple:
+    '''
+    Pre-Precesses the data.
+    Remove outliers and missing values, Balances the classes ratio.
+    ~~
+    Parameters:
+    - `data`:pd.DataFrane ->
+    - `transformer`:object -> The pre-processor object.
+    - `fit`:bool -> A flag to specify the pre-processing mode for either fit and transform then return the processed data, or to return the fitted transformer only.
+    __
+    Return:
+    If fit = True.
+    - `transformer`:pre-processor -> Fitted transformer.
+    if fit = False.
+    - `data_dict`:dict, `transformer`:pre-processor -> A dictionary contatins all data splits train/val/test X/y, fitted transformer.
+    '''
     data = clean_data(data)
 
-    data = preprocessing(data)
-    if test:
-        return data
     X = data.drop(columns=['is_hazardous'])
     y = data['is_hazardous']
 
-    return X, y
+    X_train, X_test, y_train, y_test = \
+    train_test_split(X, y, test_size=0.2,
+                        random_state=RANDOM_STATE,
+                        shuffle=True, stratify=y)
+
+    X_train, X_val, y_train, y_val = \
+    train_test_split(X_train, y_train, test_size=0.25,
+                        random_state=RANDOM_STATE,
+                        shuffle=True, stratify=y_train)
+
+    transformer.fit(X_train)
+    if fit:
+        return transformer
+
+    X_train = transformer.transform(X_train)
+    X_test = transformer.transform(X_test)
+    X_val = transformer.transform(X_val)
+
+    data_dict = {
+        'train': (X_train, y_train),
+        'val': (X_val, y_val),
+        'test': (X_test, y_test)
+    }
+
+    return data_dict, transformer
 
 
-def train(X_train, y_train):
+def train(X_train, y_train, X_val, y_val, load=False):
     """
     Train the model, save it, and perform GridSearchCV.
     """
+
     # Load/init model
-    model = initialize_model()
-
-    # Define parameter grid
-    # param_grid = define_param_grid()
-
-    # Model fit..
-    results = model_fit(model=model, X=X_train, y=y_train)
-
-    # Perform grid search to find the best model
-    # grid_search = perform_grid_search(pipeline, param_grid, X_train, y_train)
+    if load:
+        model = initialize_model(load=load)
+        results = {
+            'model':model,
+            'score':model.score(X_val, y_val)
+        }
+    else:
+        # Model fit..
+        results = model_fit(model=model, X=X_train, y=y_train, X_val=X_val, y_val=y_val)
 
     # Validation Score
-    # validation_score = np.float32(grid_search.best_score_)
     validation_score = results['score']
     print(f"Validation Score: {validation_score:.4f}")
 
@@ -50,9 +82,8 @@ def evaluate(model, X_test, y_test):
     """
     Evaluate the model and return the evaluation score.
     """
-    # Evaluate the model
-    # evaluation_score = np.float32(evaluate_best_model(grid_search, X_test, y_test))
 
+    print("Model Evaluation...")
     evaluation_score = evaluate_model(model=model, X_test=X_test, y_test=y_test)
 
     print(f"Evaluation Score: {evaluation_score:.4f}")
@@ -60,15 +91,15 @@ def evaluate(model, X_test, y_test):
     return evaluation_score
 
 
-def predict(model, feature_input):
+def predict(X, model=None):
     """
     Load the model, predict based on feature input, and return the prediction.
     """
-    # Predict using the model (alternatively load the saved model)
-    # grid_search = joblib.load('best_model.pkl')  # If loading from a saved file
 
+    if model is None:
+        initialize_model(load=True)
     # Predict the result
-    prediction = model.predict([feature_input])
+    prediction = model.predict(X)
 
     # Prediction as boolean
     prediction_bool = bool(prediction[0])
@@ -76,22 +107,21 @@ def predict(model, feature_input):
 
     return prediction_bool
 
-# Example of loading features from an API (pseudo-code, replace with actual API function)
-# feature_input = get_features_from_api()
-# predict_with_model(grid_search, feature_input)
-
-
 
 if __name__ == '__main__':
 
-    X, y = preprocess(False)
+    data = pd.read_csv(DATA_LOCAL_PATH)
 
-    X_train, X_test, y_train, y_test = \
-    train_test_split(X, y, test_size=0.2,
-                        random_state=RANDOM_STATE,
-                        shuffle=True, stratify=y)
+    transformer = pre_processor()
 
-    model = train(X_train, y_train)
+    data_dict, transformer = preprocess(data, transformer)
+
+    X_train, y_train = data_dict['train']
+    X_val, y_val = data_dict['val']
+    X_test, y_test = data_dict['test']
+
+
+    model = train(X_train, y_train, X_val, y_val, load=True)
 
     score = evaluate(model, X_test, y_test)
 
@@ -101,8 +131,10 @@ if __name__ == '__main__':
         'absolute_magnitude': [-73.950655],
         'relative_velocity': [-73.984365],
         'estimated_diameter_min': [40.783282],
-        'miss_distance': [40.769802]
     })
 
-    # y_pred = predict(model, X_new)
-    # print(f'Predicted Fare: {y_pred[0]}')
+    X_new = transformer.transform(X_new)
+
+    y_pred = predict(X_new, model)
+
+    # print(f'is Hazurdus?: {y_pred}')
